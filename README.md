61517a2dd521026dc3e5c1e289bca11698b24c03
Wajid Javed <wajidjaved17@gmail.com>
2020-02-26 19:14:53 +0500

    chat

---
+++ README.md
@@ -0,0 +1,25 @@
+# Electron chat sample with jQuery
+
+![](https://raw.githubusercontent.com/wiki/naga3/electron-chat/ss.gif)
+
+## Install
+
+```
+git clone https://github.com/naga3/electron-chat.git
+cd electron-chat
+npm install
+```
+
+## Start server
+
+```
+node server
+```
+
+## Start client
+
+```
+npm run chat
+```
+
+Please be multiple boot.
---
+++ main.js
@@ -0,0 +1,15 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') app.quit();
+});
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({width: 600, height: 800});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
